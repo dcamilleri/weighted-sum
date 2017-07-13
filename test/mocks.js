bdc5e4c2f5b0b0b5127249c95f455ca625d119f9
Mike Ruhlin <mike@schooladmin.com>
2017-07-13 12:37:15 -0500

    Don't blow up on 0 values

--- test/mocks.js
+++ test/mocks.js
@@ -97,10 +97,45 @@ const correctData = {
   ]
 }
 
+const dataWithZeroValue = {
+  source: [
+    {
+      id: 'Restaurant 1',
+      note: 4,
+      distance: 626
+    },
+    {
+      id: 'Restaurant 2',
+      note: 4,
+      distance: 0
+    },
+    {
+      id: 'Restaurant 3',
+      note: 3,
+      distance: 700
+    }
+  ],
+  sortOptions: {
+    note: {
+      weight: 0.4
+    },
+    distance: {
+      weight: 0.6,
+      sort: 'desc'
+    }
+  },
+  expected: [
+    'Restaurant 2',
+    'Restaurant 1',
+    'Restaurant 3'
+  ]
+}
+
 module.exports = {
   emptyData,
   wrongType,
   wrongDataType,
   errorData,
-  correctData
+  correctData,
+  dataWithZeroValue
 }
